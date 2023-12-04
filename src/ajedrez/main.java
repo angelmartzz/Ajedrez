f4bb39fb84df4162b1236b0f01c34a53a019e074
angelmartzz <144675850+angelmartzz@users.noreply.github.com>
2023-12-04 09:10:07 +0100

    Update main.java

--- src/ajedrez/main.java
+++ src/ajedrez/main.java
@@ -6,11 +6,19 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Clase principal que inicia la aplicación de ajedrez.
+ */
 public class main {
+
+    /**
+     * Método principal que inicia la aplicación creando una instancia de la clase MenuInicio.
+     *
+     * @param args Los argumentos de la línea de comandos (no se utilizan en esta aplicación).
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new MenuInicio();
         });
-        
     }
 }
