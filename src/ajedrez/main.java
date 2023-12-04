e9487dff032fd47152f96843e5a3442c2a00f437
angelmartzz <144675850+angelmartzz@users.noreply.github.com>
2023-12-04 01:45:11 +0100

    Update main.java

--- src/ajedrez/main.java
+++ src/ajedrez/main.java
@@ -1,20 +1,16 @@
 package ajedrez;
 
-public class Torre extends Pieza {
+import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	public Torre(Color color) {
-		super(color);
-	}
-
-	/*
-	 * Movimiento válido si la torre se mueve solo horizontalmente o verticalmente
-	 */
-	@Override
-	public boolean esMovimientoValido(int filaInicial, int columnaInicial, int filaFinal, int columnaFinal) {
-		int filaDiferencia = Math.abs(filaFinal - filaInicial);
-		int columnaDiferencia = Math.abs(columnaFinal - columnaInicial);
-
-		return (filaDiferencia == 0 || columnaDiferencia == 0);
-	} 
+public class main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new MenuInicio();
+        });
+        
+    }
 }
-
